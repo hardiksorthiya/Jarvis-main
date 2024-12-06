033e5666fc13fc541ca00535d7ece1d6105adc6e
hardiksorathiya <sorathiyahardik000@gmail.com>
2024-12-06 12:59:56 +0530

    image upload name issue resolve

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,28 @@ $( '.sidebar-nav a' ).on( 'click', function () {
 $(function(){
   $("#sidebar").load("inc/sidebar.html"); 
 });
+
+
+// =========================================
+
+var input = document.getElementById( 'file-upload' );
+var infoArea = document.getElementById( 'file-upload-filename' );
+
+input.addEventListener( 'change', showFileName );
+
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
+
+var input = document.getElementById( 'file-upload2' );
+var infoArea = document.getElementById( 'file-upload-filename2' );
+
+input.addEventListener( 'change', showFileName );
+
+function showFileName( event ) {
+  var input = event.srcElement;
+  var fileName = input.files[0].name;
+  infoArea.textContent = 'File name: ' + fileName;
+}
